4ec3ee3ba03d16370a7f78b13e28a22864341dbf
_rəSH_ <127571364+w0m73@users.noreply.github.com>
2023-03-19 17:12:07 -0300

    Update DoS.c

--- DoS.c
+++ DoS.c
@@ -18,8 +18,8 @@ int main(void) {
 	
 	meusocket = socket(AF_INET,SOCK_STREAM, 0);
 	alvo.sin_family = AF_INET;
-	alvo.sin_port = htons(21);
-	alvo.sin_addr.s_addr = inet_addr("192.168.0.1");
+	alvo.sin_port = htons(21); // Porta do serviço alvo
+	alvo.sin_addr.s_addr = inet_addr("192.168.0.1"); //IP do alvo
 	
 	conecta = connect(meusocket, (struct sockaddr *)&alvo, sizeof alvo);
 	if(conecta ==0){
